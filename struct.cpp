d1e2557971860179d93d5e26aa724feeb797d145
quandeptraiprovip <ttmq38@gmail.com>
2023-03-24 01:06:08 +0700

    them vl

--- struct.cpp
+++ struct.cpp
@@ -13,11 +13,90 @@ void Box1::drawBox() {
         return;
     }
 
+    start_color();
+    init_pair(1, COLOR_WHITE, COLOR_BLACK);
+    init_pair(2, COLOR_BLACK, COLOR_WHITE);
+    attron(COLOR_PAIR(1));
+
     int x = i + 1;
     int y = j + 1;
 
     for (int k = 0; k < 5; k++) {
-        move(j * 4 + k, 10 * i - 5);
+        move(y * 4 + k, 10 * x - 5);
         printw(box[k]);
+    }
+
+    attroff(COLOR_PAIR(1));
+
+    if(isSelected) {
+        attron(COLOR_PAIR(2));
+        for (int k = 0; k < 3; k++) {
+            move(y * 4 + k + 1, 10 * x - 5 + 1);
+            printw("         ");
+        }
+
+        move(j * 4 + 2, 10 * i - 5 + 5);
+        printw(&c);
+
+        attroff(COLOR_PAIR(2));
+    }else {
+        attron(COLOR_PAIR(1));
+        move(y * 4 + 2, 10 * x - 5 + 5);
+        printw(&c);
+        attroff(COLOR_PAIR(1));
+    }
+}
+
+void Box1::deleteBox() {
+    int x = j + 1, y = i + 1;
+
+    for (int k = 0; k < 5; k++) {
+        move(y * 4 + k, 10 * x - 5);
+        printw("           ");
+    }
+}
+
+void Box2::drawBox() {
+
+    start_color();
+    init_pair(1, COLOR_WHITE, COLOR_BLACK);
+    init_pair(2, COLOR_BLACK, COLOR_WHITE);
+    attron(COLOR_PAIR(1));
+
+    int x = i + 1;
+    int y = j + 1;
+
+    for (int k = 0; k < 5; k++) {
+        move(y * 4 + k, 10 * x - 5);
+        printw(box[k]);
+    }
+
+    attroff(COLOR_PAIR(1));
+
+    if(isSelected) {
+        attron(COLOR_PAIR(2));
+        for (int k = 0; k < 3; k++) {
+            move(y * 4 + k + 1, 10 * x - 5 + 1);
+            printw("         ");
+        }
+
+        move(j * 4 + 2, 10 * i - 5 + 5);
+        printw(&c);
+
+        attroff(COLOR_PAIR(2));
+    }else {
+        attron(COLOR_PAIR(1));
+        move(y * 4 + 2, 10 * x - 5 + 5);
+        printw(&c);
+        attroff(COLOR_PAIR(1));
+    }
+}
+
+void Box2::deleteBox() {
+    int x = j + 1, y = i + 1;
+
+    for (int k = 0; k < 5; k++) {
+        move(y * 4 + k, 10 * x - 5);
+        printw("           ");
     }
 }
